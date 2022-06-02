36fd15b9fb9d59d65d74085ca75cb3e3d59fe82a
saroj7 <sarojsht377@gmail.com>
2022-06-02 21:03:21 +0545

    Setting up application to connect to DB, Middleware, and json

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const connectDB = require('./db/connect');
+
 //Environment Variables
 require('dotenv').config();
 
-app.listen(process.env.PORT || port, () => console.log(`App is listening at port ${port}`));
+//Middleware
+const notFound = require('./middleware/not-found');
+
+app.use(notFound);
+app.use(express.json());
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(process.env.PORT || port, () => {
+            return console.log(`App is listening at port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
