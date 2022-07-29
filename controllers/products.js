df65822371be61a8511b7eeabebac0c15ca497e4
saroj7 <sarojsht377@gmail.com>
2022-07-29 17:42:04 +0545

    Page Field, Numeric Filter and Regular Expression

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,9 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("This is the Error");
   // const search = "woo";
-  const product = await Product.find({}).sort("-name price");
+  const product = await Product.find({ price: { $gte: 30, $lte: 100 } })
+    .sort("price")
+    .select("price");
   res.status(200).json({ nbhits: product.length, product });
 };
 
@@ -16,7 +18,7 @@ const getAllProducts = async (req, res) => {
   //   throw new Error("This is the Error");
   // }
 
-  const { featured, company, name, rating, sort } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
 
@@ -32,17 +34,48 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  if (rating) {
-    queryObject.rating = { $gte: rating };
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "eq",
+    };
+    const regEx = /\b(<|>|<=|=|>=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: value };
+      }
+    });
   }
 
+  console.log(queryObject);
+
   let result = Product.find(queryObject);
+  //Sorting
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("-rating");
   }
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
